1576a363f7a3b52fc7ec380e5159a19c8432154a
gab <gabrijelsota@gmail.com>
2021-05-05 07:20:34 +0200

    Update Program.cs

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
-using BoggedFinanceBot;
 using Geste.Messaging;
 using Geste.Services;
 using Microsoft.Extensions.DependencyInjection;
